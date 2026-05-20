13932eb7928a29080d7224c921c0c6f8e80452ca
sofvlad <sofienkovladislav@gmail.com>
2026-05-20 19:10:16 +0600

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,10 @@ Route::inertia('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::prefix('visits')->group(function () {
+        Route::get('/hourly-stats', [VisitController::class, 'hourlyStats'])->name('visits.hourly-stats');
+        Route::get('/city-stats', [VisitController::class, 'cityStats'])->name('visits.city-stats');
+    });
 });
 
 require __DIR__.'/settings.php';
-
-Route::prefix('visits')->group(function () {
-    Route::get('/hourly-stats', [VisitController::class, 'hourlyStats'])->name('visits.hourly-stats');
-    Route::get('/city-stats', [VisitController::class, 'cityStats'])->name('visits.city-stats');
-    Route::get('/stats', [VisitController::class, 'stats'])->name('visits.stats');
-});
